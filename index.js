fdd57b1f80531fe9075131a7b725f61217d779e7
Antony Nyagah <antonynyagah@antonys-MBP.attlocal.net>
2019-05-11 12:09:46 -0400

    2nd index commit

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const server = require('./server');
+const postsRoutes = require('./postsRoutes/postsRoutes');
+
+const server = express();
+const parser = express.json();
+server.use(parser);
+server.use('/api/posts', postsRoutes);
+
+
 
 server.listen(4000, () => {
   console.log("listening in port 4000... ");
